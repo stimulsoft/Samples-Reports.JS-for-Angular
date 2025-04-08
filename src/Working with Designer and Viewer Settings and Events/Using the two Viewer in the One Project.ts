602615c5e35831edde306e2f65498d2a30d4397a
Serge Puhov <musevitebsk@gmail.com>
2025-04-08 11:20:26 +0200

    Improved appearance of samples for Angular
    https://github.com/stimulsoft/Stimulsoft.Reports/issues/17087

--- src/Working with Designer and Viewer Settings and Events/Using the two Viewer in the One Project.ts
+++ src/Working with Designer and Viewer Settings and Events/Using the two Viewer in the One Project.ts
@@ -8,8 +8,12 @@ import { Viewer, Stimulsoft } from "stimulsoft-reports-js-angular/viewer";
         <div class="container">
             <h4>This sample demonstrates how to add two viewers to one page:</h4>
             <div class="container-viewer">
-                <sti-viewer [report]="report1"></sti-viewer>
-                <sti-viewer [report]="report2"></sti-viewer>
+                <div>
+                    <sti-viewer [report]="report1" [options]="viewerOptions"></sti-viewer>
+                </div>
+                <div>
+                    <sti-viewer [report]="report2" [options]="viewerOptions"></sti-viewer>
+                </div>
             </div>
         </div>
     `,
@@ -19,8 +23,13 @@ import { Viewer, Stimulsoft } from "stimulsoft-reports-js-angular/viewer";
 export class UsingTheTwoViewerInTheOneProject {
     report1: Stimulsoft.Report.StiReport;
     report2: Stimulsoft.Report.StiReport;
+    viewerOptions: Stimulsoft.Viewer.StiViewerOptions;
 
     constructor() {
+        this.viewerOptions = new Stimulsoft.Viewer.StiViewerOptions();
+        this.viewerOptions.appearance.scrollbarsMode = true;
+        this.viewerOptions.height = "100vh";
+
         this.report1 = new Stimulsoft.Report.StiReport();
         this.report1.loadFile("Reports/SimpleList.mrt");
 
